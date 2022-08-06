0bae7fdca8828b2de1753a91052ecc0d5a45053b
aslanmagamaev <aslanmagamaev@gmail.com>
2022-08-06 20:50:45 +0300

    Bot simple functions added

---
+++ src/main/java/finance/bot/FinanceHelperBotApplication.java
@@ -0,0 +1,11 @@
+package finance.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceHelperBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceHelperBotApplication.class, args);
+    }
+}
---
+++ src/main/java/finance/bot/config/BotConfig.java
@@ -0,0 +1,16 @@
+package finance.bot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource(value = "classpath:application.properties")
+public class BotConfig {
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${bot.key}")
+    private String token;
+}
