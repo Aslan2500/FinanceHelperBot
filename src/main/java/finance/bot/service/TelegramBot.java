9d98fd1e81f3f4a5fe1a46ab1031b09c9222fe31
aslanmagamaev <aslanmagamaev@gmail.com>
2022-09-29 17:51:24 +0300

    Trash deleted, main functionality released

--- src/main/java/finance/bot/service/TelegramBot.java
+++ src/main/java/finance/bot/service/TelegramBot.java
@@ -3,7 +3,10 @@ package finance.bot.service;
 import com.vdurmont.emoji.EmojiParser;
 import finance.bot.config.BotConfig;
 import finance.bot.entity.User;
+import finance.bot.entity.texts.CommandTexts;
 import finance.bot.repository.UserRepository;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -13,12 +16,14 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -27,16 +32,12 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Autowired
     private UserRepository userRepository;
     final BotConfig config;
-    private static final String HELP_TEXT = "Bot for financial purposes. \n\nIt is still in progress.";
 
     public TelegramBot(BotConfig config) {
         this.config = config;
         List<BotCommand> listOfCommands = new ArrayList<>();
-        listOfCommands.add(new BotCommand("/start", "get welcome message"));
-        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
-        listOfCommands.add(new BotCommand("/deletedata", "delete my data"));
-        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
-        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        listOfCommands.add(new BotCommand("/start", "Press to start using this bot"));
+        listOfCommands.add(new BotCommand("/help", "How to use this bot"));
         try {
             this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
         } catch (TelegramApiException e) {
@@ -64,8 +65,8 @@ public class TelegramBot extends TelegramLongPollingBot {
                     registerUser(update.getMessage());
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                 }
-                case "/help" -> sendMessage(chatId, HELP_TEXT);
-                default -> sendMessage(chatId, "Sorry, command was not recognized");
+                case "/help" -> sendMessage(chatId, CommandTexts.HELP_TEXT.toString());
+                default -> stockReport(chatId, update.getMessage());
             }
         }
     }
@@ -89,23 +90,29 @@ public class TelegramBot extends TelegramLongPollingBot {
         sendMessage(chatId, answer);
     }
 
+    private void stockReport(long chatId, Message message) {
+        try {
+            URL url = new URL("https://financialmodelingprep.com/api/v3/income-statement/" + message.getText() + "?apikey=83cf4ee49eed09add7c13f7049ccb5ad");
+            String json = IOUtils.toString(url, StandardCharsets.UTF_8);
+            JSONArray jsonArray = new JSONArray(json);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                sendMessage(
+                        chatId, "Link - " + jsonArray.getJSONObject(i).get("finalLink").toString() + "\n" +
+                                "Revenue: " + jsonArray.getJSONObject(i).get("revenue") + "\n" +
+                                "Eps: " + jsonArray.getJSONObject(i).get("eps") + "\n" +
+                                "Ebitda: " + jsonArray.getJSONObject(i).get("ebitda") + "\n" +
+                                "Net Income: " + jsonArray.getJSONObject(i).get("netIncome")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-        List<KeyboardRow> keyBoardRows = new ArrayList<>();
-        KeyboardRow row = new KeyboardRow();
-        row.add("weather");
-        row.add("random");
-        keyBoardRows.add(row);
-        row = new KeyboardRow();
-        row.add("register");
-        row.add("check data");
-        row.add("delete my data");
-        keyBoardRows.add(row);
-        keyboardMarkup.setKeyboard(keyBoardRows);
-        message.setReplyMarkup(keyboardMarkup);
         try {
             execute(message);
         } catch (TelegramApiException e) {
